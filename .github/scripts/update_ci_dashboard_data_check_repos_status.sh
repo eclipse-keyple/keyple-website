ab56b52e3968b717287e91c652d28c9eb6abbc62
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2022-07-26 19:15:27 +0200

    ci: fix dashboard update (#110)

--- .github/scripts/update_ci_dashboard_data_check_repos_status.sh
+++ .github/scripts/update_ci_dashboard_data_check_repos_status.sh
@@ -6,21 +6,26 @@
 token=$1
 repository_name=$2
 
+filter_keyple_website=""
+
+if [ "$repository_name" = "keyple-website" ]; then
+  filter_keyple_website=" -e \"pushed_at\""
+fi
+
 echo "Checking repository "$repository_name"..."
 
 # Compare hash value of the API result by excluding these fields:
-# - "pushed_at": Cyclic commit when gh-pages branch is update.
 # - "size": Unknown reason of size changing.
-# - "admin", "maintain", "push", "triage", "pull", "...": Access rights to the keyple-website repo differ when owner is eclipse.
+# - "pushed_at": Cyclic commit when gh-pages branch is update (keyple-website).
 
 github_hash=`curl --request GET \
           --url https://api.github.com/repos/eclipse/$repository_name \
           --header "authorization: Bearer $token" \
-          --header "content-type: application/json" | grep -v -e "pushed_at" -e "size" -e "\"admin\":" -e "maintain" -e "push" -e "triage" -e "pull" -e "allow_squash_merge" -e "allow_merge_commit" -e "allow_rebase_merge" -e "allow_auto_merge" -e "delete_branch_on_merge" -e "allow_update_branch" | md5sum | cut -d ' ' -f 1`
+          --header "content-type: application/json" | grep -v -e "size"$filter_keyple_website | md5sum | cut -d ' ' -f 1`
 
 dashboard_hash=`curl --request GET \
           --url https://keyple.org/dashboard/$repository_name"_.json" \
-          --header "content-type: application/json" | grep -v -e "pushed_at" -e "size" -e "\"admin\":" -e "maintain" -e "push" -e "triage" -e "pull" -e "allow_squash_merge" -e "allow_merge_commit" -e "allow_rebase_merge" -e "allow_auto_merge" -e "delete_branch_on_merge" -e "allow_update_branch" | md5sum | cut -d ' ' -f 1`
+          --header "content-type: application/json" | grep -v -e "size"$filter_keyple_website | md5sum | cut -d ' ' -f 1`
 
 echo "github_hash="$github_hash
 echo "dashboard_hash="$dashboard_hash
