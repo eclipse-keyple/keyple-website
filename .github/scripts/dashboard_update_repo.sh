68204f89e623980d0eb55344cfe0115af6d547ed
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-01-30 13:38:02 +0100

    site: fix dashboard (#174)

--- .github/scripts/dashboard_update_repo.sh
+++ .github/scripts/dashboard_update_repo.sh
@@ -11,10 +11,10 @@ echo "Update the dashboard data of repository $repository_name..."
 
 echo -n "[\"$repository_name\","$doc_flag","$ci_status_flag"]" >> dashboard/repository_list.json
 
-../.github/scripts/dashboard_update_repo_curl.sh $organization $token $repository_name "" ""
-../.github/scripts/dashboard_update_repo_curl.sh $organization $token $repository_name "/branches" "branches"
-../.github/scripts/dashboard_update_repo_curl.sh $organization $token $repository_name "/issues" "issues"
-../.github/scripts/dashboard_update_repo_curl.sh $organization $token $repository_name "/pulls" "pulls"
-../.github/scripts/dashboard_update_repo_curl.sh $organization $token $repository_name "/releases/latest" "releases_latest"
-../.github/scripts/dashboard_update_repo_curl.sh $organization $token $repository_name "/tags" "tags"
-../.github/scripts/dashboard_update_repo_curl.sh $organization $token $repository_name "/commits/"$branch_name"/status" "commits_status"
+../.github/scripts/dashboard_update_repo_curl.sh $token $organization $repository_name "" ""
+../.github/scripts/dashboard_update_repo_curl.sh $token $organization $repository_name "/branches" "branches"
+../.github/scripts/dashboard_update_repo_curl.sh $token $organization $repository_name "/issues" "issues"
+../.github/scripts/dashboard_update_repo_curl.sh $token $organization $repository_name "/pulls" "pulls"
+../.github/scripts/dashboard_update_repo_curl.sh $token $organization $repository_name "/releases/latest" "releases_latest"
+../.github/scripts/dashboard_update_repo_curl.sh $token $organization $repository_name "/tags" "tags"
+../.github/scripts/dashboard_update_repo_curl.sh $token $organization $repository_name "/commits/"$branch_name"/status" "commits_status"
