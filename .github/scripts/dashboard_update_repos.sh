ee3a9a329704e59f82c8db7634c945613352dae9
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-01-30 13:36:37 +0100

    site: fix dashboard

--- .github/scripts/dashboard_update_repos.sh
+++ .github/scripts/dashboard_update_repos.sh
@@ -8,70 +8,70 @@ mkdir dashboard/
 echo "{ \"datetime\":\"`date --utc --iso-8601=seconds`\"}" > dashboard/datetime.json
 
 echo "[" >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple master false false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple master false false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-card-calypso-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-card-calypso-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-card-calypso-crypto-legacysam-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-card-calypso-crypto-legacysam-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-card-generic-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-card-generic-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse-keyple $token keyple-common-java-api main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-common-java-api main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-distributed-local-java-api main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-distributed-local-java-api main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-distributed-local-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-distributed-local-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-distributed-network-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-distributed-network-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-distributed-remote-java-api main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-distributed-remote-java-api main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-distributed-remote-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-distributed-remote-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-integration-java-test main false true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-integration-java-test main false true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-java-example main false true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-java-example main false true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-plugin-android-nfc-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-plugin-android-nfc-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-plugin-android-omapi-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-plugin-android-omapi-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-plugin-java-api main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-plugin-java-api main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-plugin-pcsc-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-plugin-pcsc-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-plugin-stub-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-plugin-stub-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-plugin-cardresource-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-plugin-cardresource-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-service-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-service-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-service-resource-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-service-resource-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-util-java-lib main true true
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-util-java-lib main true true
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-website master false false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-website master false false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-common-cpp-api master true false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-common-cpp-api master true false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-util-cpp-lib master true false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-util-cpp-lib master true false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-service-cpp-lib master true false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-service-cpp-lib master true false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-service-resource-cpp-lib master true false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-service-resource-cpp-lib master true false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-plugin-cpp-api master true false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-plugin-cpp-api master true false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-plugin-stub-cpp-lib master true false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-plugin-stub-cpp-lib master true false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-plugin-pcsc-cpp-lib master true false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-plugin-pcsc-cpp-lib master true false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-card-generic-cpp-lib master true false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-card-generic-cpp-lib master true false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-card-calypso-cpp-lib master true false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-card-calypso-cpp-lib master true false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-cpp-example master false false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-cpp-example master false false
 echo "," >> dashboard/repository_list.json
-../.github/scripts/dashboard_update_repo.sh eclipse $token keyple-cpp-meta master false false
+../.github/scripts/dashboard_update_repo.sh $token eclipse-keyple keyple-cpp-meta master false false
 echo "" >> dashboard/repository_list.json
 echo "]" >> dashboard/repository_list.json
